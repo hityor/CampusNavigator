9efcd37f5f0392fe4c35d42f53e9c4420599b979
Egor <gontarevegorboss@gmail.com>
2026-03-25 01:40:58 +0700

    add A* pathfinding and render path with polyline
    (cherry picked from commit ba82e943d57ca3542a8b5edb6a6d22587ab02360)

--- app/src/main/java/com/example/campusnavigator/MapScreen.kt
+++ app/src/main/java/com/example/campusnavigator/MapScreen.kt
@@ -1,26 +1,38 @@
 package com.example.campusnavigator
 
 import android.graphics.Bitmap
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.viewinterop.AndroidView
-import org.maplibre.android.MapLibre
-import org.maplibre.android.camera.CameraPosition
-import org.maplibre.android.geometry.LatLng
-import org.maplibre.android.maps.MapView
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.ui.unit.dp
-import androidx.compose.material3.*
-import androidx.compose.foundation.layout.*
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import org.maplibre.android.MapLibre
+import org.maplibre.android.annotations.MarkerOptions
+import org.maplibre.android.annotations.PolylineOptions
+import org.maplibre.android.camera.CameraPosition
+import org.maplibre.android.geometry.LatLng
 import org.maplibre.android.geometry.LatLngQuad
+import org.maplibre.android.maps.MapView
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -28,10 +40,15 @@ import org.maplibre.android.geometry.LatLngQuad
 fun MapScreen(gridMap: GridMap, gridBitMap: Bitmap, latLngQuad: LatLngQuad) {
     val scaffoldState = rememberBottomSheetScaffoldState()
 
+    var startCell by remember { mutableStateOf<GridCell?>(null) }
+    var finishCell by remember { mutableStateOf<GridCell?>(null) }
+
+    var path by remember { mutableStateOf<List<GridCell>>(emptyList()) }
+
     BottomSheetScaffold(
         scaffoldState = scaffoldState,
         sheetPeekHeight = 100.dp,
-        sheetDragHandle =  null,
+        sheetDragHandle = null,
         sheetContent = {
             Column(
                 modifier = Modifier
@@ -53,7 +70,17 @@ fun MapScreen(gridMap: GridMap, gridBitMap: Bitmap, latLngQuad: LatLngQuad) {
                 Spacer(modifier = Modifier.height(20.dp))
 
 
-                Button(onClick = {}) { Text("Запустить A*") }
+                Button(onClick = {
+                    if (startCell != null && finishCell != null) {
+                        val foundPath = findPath(startCell!!, finishCell!!, gridMap)
+                        path = foundPath ?: emptyList()
+                        if (path.isNotEmpty()) {
+                            Log.d("astar", "path length: ${path.size}")
+                        } else {
+                            Log.d("astar", "path is not found")
+                        }
+                    }
+                }, enabled = startCell != null && finishCell != null) { Text("Запустить A*") }
 
                 Spacer(modifier = Modifier.weight(1f))
 
@@ -69,6 +96,30 @@ fun MapScreen(gridMap: GridMap, gridBitMap: Bitmap, latLngQuad: LatLngQuad) {
             gridMap = gridMap,
             gridBitmap = gridBitMap,
             latLngQuad = latLngQuad,
+            onCellSelected = { cell ->
+                when {
+                    startCell == null -> {
+                        startCell = cell
+                        path = emptyList()
+                    }
+
+                    finishCell == null -> {
+                        finishCell = cell
+                        path = emptyList()
+                    }
+
+                    else -> {
+                        startCell = cell
+                        finishCell = null
+                        path = emptyList()
+                    }
+
+                }
+
+            },
+            startCell = startCell,
+            finishCell = finishCell,
+            path = path,
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
@@ -77,12 +128,55 @@ fun MapScreen(gridMap: GridMap, gridBitMap: Bitmap, latLngQuad: LatLngQuad) {
 }
 
 @Composable
-fun MapViewContainer(gridMap: GridMap, gridBitmap: Bitmap, latLngQuad: LatLngQuad, modifier: Modifier = Modifier) {
+fun MapViewContainer(
+    gridMap: GridMap,
+    gridBitmap: Bitmap,
+    latLngQuad: LatLngQuad,
+    onCellSelected: (GridCell) -> Unit,
+    startCell: GridCell?,
+    finishCell: GridCell?,
+    path: List<GridCell>,
+    modifier: Modifier = Modifier
+) {
     val context = LocalContext.current
     remember { MapLibre.getInstance(context) }
 
     AndroidView(
         modifier = modifier,
+        update = { mapView ->
+            mapView.getMapAsync { map ->
+                map.clear()
+
+                if (startCell != null) {
+                    val pos = gridCellToLatLng(startCell.row, startCell.col, gridMap)
+                    map.addMarker(
+                        MarkerOptions().position(pos).title("Start")
+                    )
+
+                }
+
+                if (finishCell != null) {
+                    val pos = gridCellToLatLng(finishCell.row, finishCell.col, gridMap)
+                    map.addMarker(
+                        MarkerOptions().position(pos).title("Finish")
+                    )
+                }
+
+                if (path.isNotEmpty()) {
+                    val pathPoints = path.map { point ->
+                        gridCellToLatLng(point.row, point.col, gridMap)
+                    }
+
+                    map.addPolyline(
+                        PolylineOptions()
+                            .addAll(pathPoints)
+                            .width(3f)
+                            .color(android.graphics.Color.BLUE)
+                    )
+                }
+            }
+
+        },
         factory = { ctx ->
             MapView(ctx).apply {
                 onCreate(android.os.Bundle())
@@ -96,7 +190,10 @@ fun MapViewContainer(gridMap: GridMap, gridBitmap: Bitmap, latLngQuad: LatLngQua
                         )
                         style.addSource(imageSource)
 
-                        val rasterLayer = org.maplibre.android.style.layers.RasterLayer("grid-mask-layer", "grid-mask-source")
+                        val rasterLayer = org.maplibre.android.style.layers.RasterLayer(
+                            "grid-mask-layer",
+                            "grid-mask-source"
+                        )
                         style.addLayer(rasterLayer)
                     }
 
@@ -105,11 +202,11 @@ fun MapViewContainer(gridMap: GridMap, gridBitmap: Bitmap, latLngQuad: LatLngQua
                         val tappedCell = epsg3857ToGridCell(x, y, gridMap)
 
 
-                        val selectedCell = findNearestWalkableCell(tappedCell, gridMap, maxRadius = 3)
+                        val selectedCell =
+                            findNearestWalkableCell(tappedCell, gridMap, maxRadius = 3)
 
                         if (selectedCell != null) {
-                            val finalPos = gridCellToLatLng(selectedCell.row, selectedCell.col, gridMap)
-                            map.addMarker(org.maplibre.android.annotations.MarkerOptions().position(finalPos))
+                            onCellSelected(selectedCell)
                         }
                         true
                     }
