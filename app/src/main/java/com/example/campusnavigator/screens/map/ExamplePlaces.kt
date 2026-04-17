76f55e2f030bd122e85c80f45a8f6348088b51a8
Egor <gontarevegorboss@gmail.com>
2026-04-18 02:34:46 +0700

    add several new places

--- app/src/main/java/com/example/campusnavigator/screens/map/ExamplePlaces.kt
+++ app/src/main/java/com/example/campusnavigator/screens/map/ExamplePlaces.kt
@@ -3,12 +3,15 @@ package com.example.campusnavigator.screens.map
 import com.example.campusnavigator.screens.map.models.FoodPlace
 
 val sampleFoodPlaces = listOf(
-    FoodPlace("1", "Main Cafeteria", 56.4694, 84.9468),
-    FoodPlace("2", "Starbooks", 56.4695, 84.9461),
-    FoodPlace("3", "Siberian Pancakes", 56.4693, 84.9466),
-    FoodPlace("4", "Yarche1", 56.4739, 84.9446),
-    FoodPlace("5", "Yarche2", 56.4714, 84.9536),
-    FoodPlace("6", "Second Building Cafe", 56.4686, 84.9451),
-    FoodPlace("7", "Abrikos", 56.4714, 84.9411),
-    FoodPlace("8", "Rostics", 56.4691, 84.9513)
+    FoodPlace("1", "Столовая ТГУ", 56.4694, 84.9468),
+    FoodPlace("2", "Старбукс", 56.4695, 84.9461),
+    FoodPlace("3", "Сибирские блины", 56.4693, 84.9466),
+    FoodPlace("4", "Ярче 1", 56.4739, 84.9446),
+    FoodPlace("5", "Ярче 2", 56.4714, 84.9536),
+    FoodPlace("6", "Кафе второго корпуса", 56.4686, 84.9451),
+    FoodPlace("7", "Абрикос", 56.4714, 84.9411),
+    FoodPlace("8", "Ростикс", 56.4691, 84.9513),
+    FoodPlace("9", "СырБор", 56.4707, 84.9461),
+    FoodPlace("10", "Минутка", 56.4693, 84.9468),
+    FoodPlace("11", "Магнолия", 56.4732, 84.9445),
 )
